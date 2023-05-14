6bfcfb8a12fb52575493572cf7d95a9d9ad69b6b
Dami Adesina <damiadesina87@gmail.com>
2023-05-13 20:55:18 -0500

    read data

---
+++ data_ingestion/ingest.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def get_data(f):
+    """
+    This is a function that takes a file path and read in data
+    with pandas
+    : f: filepath
+    : df: resulting data
+    """
+    
+    df = pd.read_csv(f)
+
+    return df
